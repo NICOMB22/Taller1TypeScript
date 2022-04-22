f5981cde6342f283b2153b39e3be3fd0a413fe58
Nicolas <n.maldonado2@uniandes.edu.co>
2022-04-21 22:26:49 -0500

    cambios finales

--- serie.ts
+++ serie.ts
@@ -2,7 +2,8 @@
 
 export class Serie{
 
-    constructor(public numero: number, public name: string, public channel: string, public seasons: number)
+    constructor(public numero: number, public name: string, public channel: string, public seasons: number,
+        public description: string, public link: string, public image: string)
     {
 
     }
